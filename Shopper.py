6875c4875c0b1b37937e8a95be836197f42607a1
borbazzz <106965750+borbazzz@users.noreply.github.com>
2022-06-07 05:35:20 -0300

    Update Shopper.py

--- Shopper.py
+++ Shopper.py
@@ -5,6 +5,7 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
+import numpy as np
 import json
 
 # Pegar os dados HTML a partir da URL
@@ -12,7 +13,7 @@ url = "https://programada.shopper.com.br/shop-cn/alimentos/"
 
 option = Options()
 option.headless = True
-driver = webdriver.Firefox(options=option)
+driver = webdriver.Firefox()
 
 driver.get(url)
 time.sleep(5)  # in seconds
@@ -26,15 +27,43 @@ time.sleep(20)  # in seconds
 element = driver.find_element_by_xpath('/html/body/div[1]/div[8]/div')
 html_content = element.get_attribute('outerHTML')
 
+#print(html_content)
+
 # Parsear o conteúdo HTML com o BeautifulSoup
 soup = BeautifulSoup(html_content, 'html.parser')
+
 driver.quit()
-alimentos = soup.findAll('p', {'class': 'sc-DJmSI kXVaHC'})
-print(alimentos)
+
+#alimentos = soup.findAll('p', {'class': 'sc-DJmSI kXVaHC'})
+#print(alimentos)
+
+#convert output to new array, check length
+array = np.asarray(soup)
+#print(len(array))
+
+#convert new array to dataframe
+df = pd.DataFrame(array[0,1])
+
+print(df)
+
+#export data
+df.to_csv('/Users/rodrigo-borba/desktop/board_members.csv')
+
+
+#print(type(alimentos))
+
+#alimentos = soup.findAll("div", class_="sc-cZbDLb eFiuxn")
+
+#print(soup)
 
 # Estruturar conteúdo em um Data Frame - Pandas
 
-#df_full = pd.read_html(alimentos)
+#df_full = pd.read_html(str(alimentos))[0]
 
 #print(df_full)
 
+############################################
+
+#headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"}
+
+#site = requests.get(URL, headers=headers)
